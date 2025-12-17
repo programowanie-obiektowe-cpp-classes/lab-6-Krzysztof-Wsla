1d2a902bf3b997604f283fefe55e194c29112f1d
Krzysztof Wszoła <krzyzak864@gmail.com>
2025-12-17 05:19:13 +0100

    p1

--- foo.hpp
+++ foo.hpp
@@ -5,12 +5,15 @@
 #include <algorithm>
 #include <list>
 #include <vector>
+#include <iostream>
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    std::vector< char > retval(people.size());
+    std::vector< char > retval(people.size()); 
 
-    // Twoja implementacja tutaj
+    std::for_each(people.begin(), people.end(), [](Human& p) { return p.birthday(); });
+
+    std::transform(people.begin(), people.end(), retval.rbegin(), [](const Human& p) {if (p.isMonster()) return 'n'; else return 'y';});
 
     return retval;
 }
